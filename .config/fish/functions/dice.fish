471167a035dd380c0217d4c26b1da4c55c32cb1a
iason <iason.papandreou@foth.dev>
2024-08-07 01:28:01 +0200

    extended dice funtion to look in different locations for dice.py and direct to the dice.py gitlab project if its not found there

--- .config/fish/functions/dice.fish
+++ .config/fish/functions/dice.fish
@@ -1,3 +1,11 @@
 function dice
-  python ~/dnd/dice.py
+  if test -e ~/dnd/dice.py
+    python ~/dnd/dice.py
+  else if test -e ~/dice.py
+    python ~/dice.py
+  else if test -e ~/dyce-goblin/dice.py
+    python ~/dice.py
+  else
+    echo "Could not find dice.py under ~/dnd/dice.py, ~/dyce-goblin/dice.py or ~/dice.py\n Consider getting it from https://gitlab.com/iason-pap/dyce-goblin" 
+  end
 end
